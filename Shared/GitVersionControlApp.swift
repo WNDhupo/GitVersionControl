86772b7d4ad990d9edcff1499127a7ec61f564e1
wd <731231777@qq.com>
2021-04-16 14:28:27 +0800

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by by4hp on 2021/4/16.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
